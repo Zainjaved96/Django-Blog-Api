dd0978548ef134cd6d47cd00f182c3b393583bf9
Zain <zainjaved965@gmail.com>
2023-07-03 19:13:16 +0500

    redirected password reset confirm email to frontend

--- main/user/urls.py
+++ main/user/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from .views import custom_password_reset_redirect
 urlpatterns = [
 
     path('', include('djoser.urls')),
-
+    path('password-confirm/<str:uid>/<str:token>/', custom_password_reset_redirect,
+         name='custom_password_reset_redirect'),
 ]
--- main/user/views.py
+++ main/user/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def custom_password_reset_redirect(request, uid, token):
+    redirect_url = f'http://localhost:3000/password-confirm?uuid={uid}&token={token}'
+    return redirect(redirect_url)
